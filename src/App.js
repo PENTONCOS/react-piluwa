4f781b2c0a8f53c0b023c9bd73fb6fcfd25d2149
jiapandong <515327098.com>
2020-03-20 17:10:39 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Button } from 'antd';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      看到这段文字说明react程序运行没有问题
+      <hr/>
+      <Button type="primary">显示此按钮说明antd运行成功</Button>
     </div>
   );
 }
